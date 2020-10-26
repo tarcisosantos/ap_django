5a1d55d9dfdd75b67800bc0c7c14ae8bdc329161
tarcisosantos <bentessantostarciso@gmail.com>
2020-10-26 02:40:38 -0300

    Contexto de Template -1

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo':'Vídeo Aperitivo: Motivação', 'vimeo_id': 251224475}
+    video = {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 251224475}
     return render(request, 'aperitivos/video.html', context={'video': video})
