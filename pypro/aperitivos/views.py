9ecfb77a334dac74c9185021903a06f52164f40d
tarcisosantos <bentessantostarciso@gmail.com>
2020-10-26 14:34:03 -0300

    filtros 2

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def video(request, slug):
     videos = {
         'motivacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 251224469},
-        'instalacao-windows': {'titulo': '', 'vimeo_id': 251224470},
+        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 251224470},
     }
     video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
