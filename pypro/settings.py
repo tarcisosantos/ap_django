39e2fe947c3279ab6b753ce1092b5a898219510e
tarcisosantos <bentessantostarciso@gmail.com>
2020-11-16 20:26:00 -0300

    Funcionalidade de Turmas

--- pypro/settings.py
+++ pypro/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'pypro.base',
+    'pypro.turmas',
     'pypro.aperitivos',
     'pypro.modulos',
     'ordered_model',
